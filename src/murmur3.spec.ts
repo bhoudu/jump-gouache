333de3f8be959231b8f8074311d76f4da28546cb
bhoudu <benjamin.houdu@gmail.com>
2021-05-09 18:11:17 +0200

    :recycle: refactor module

---
+++ src/murmur3.spec.ts
@@ -0,0 +1,9 @@
+import { murmurConsistentHash } from "./murmur3";
+
+describe('hashingUtil', () => {
+  it('Test jump consistent hash function with MurmurHash3', () => {
+    const index32 = murmurConsistentHash('gou4che4ftwuEQzP6SsUR89gbY2', 100);
+    expect(index32.bucket).toEqual(46);
+    expect(index32.hash).toEqual(1842701211);
+  });
+});
---
+++ src/murmur3.ts
@@ -0,0 +1,26 @@
+import murmurHash3 from 'murmurhash3js';
+import { jump } from "./jump";
+
+export interface StringHashResult<T> {
+  hash: T;
+  bucket: number;
+  bucketCount: number;
+}
+
+/**
+ * Jump consistent hash function based on a string input.
+ * As jump algorithm requires a 32 or 64 bit long integer, the string input is hashed thanks to MurmurHash3.
+ * It is easier to hash an object into a string in most cases, so this function helps.
+ *
+ * @param input as a string
+ * @param bucketCount as a positive integer to indicate how many buckets are valid to route inputs to
+ * @return computed hash and bucket
+ */
+export function murmurConsistentHash(input: string, bucketCount: number): StringHashResult<number> {
+  const input32: number = murmurHash3.x86.hash32(input);
+  return {
+    hash: input32,
+    bucket: jump(input32, bucketCount),
+    bucketCount,
+  };
+}
