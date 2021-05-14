a1f4fb09070c5180f57abfc2594b465bfd31f54c
bhoudu <benjamin.houdu@gmail.com>
2021-05-14 19:05:38 +0200

    test!: clean tests

--- src/murmur3.spec.ts
+++ src/murmur3.spec.ts
@@ -1,6 +1,6 @@
 import { murmurConsistentHash } from "./murmur3";
 
-describe('hashingUtil', () => {
+describe('murmur3', () => {
   it('Test jump consistent hash function with MurmurHash3', () => {
     const index32 = murmurConsistentHash('gou4che4ftwuEQzP6SsUR89gbY2', 100);
     expect(index32.bucket).toEqual(46);
