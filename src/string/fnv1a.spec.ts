504c9744e39737b755c5f687afcc0352975a6d6c
bhoudu <benjamin.houdu@gmail.com>
2021-05-22 14:15:18 +0200

    test: add default value test

--- src/string/fnv1a.spec.ts
+++ src/string/fnv1a.spec.ts
@@ -5,8 +5,13 @@ describe('fnv1a', () => {
     const index32 = fnvConsistentHash('gou4che4ftwuEQzP6SsUR89gbY2', 100, 'FNV1A_32');
     expect(index32.bucket).toEqual(39);
     expect(index32.hash).toEqual(2566704832);
+
     const index64 = fnvConsistentHash('gou4che4ftwuEQzP6SsUR89gbY2', 100, 'FNV1A_64');
     expect(index64.bucket).toEqual(69);
     expect(index64.hash + '').toEqual('11786985376615240320');
+
+    const indexDefault = fnvConsistentHash('gou4che4ftwuEQzP6SsUR89gbY2', 100);
+    expect(indexDefault.bucket).toEqual(69);
+    expect(indexDefault.hash + '').toEqual('11786985376615240320');
   });
 });
