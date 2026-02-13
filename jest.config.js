bdb85ee7087f1d6211023eb8d8c3c85fb79d19d5
Benjamin Houdu <benjamin.houdu@gmail.com>
2026-02-13 21:35:17 +0100

    :zap: format files

--- jest.config.js
+++ jest.config.js
@@ -1,37 +1,24 @@
 module.exports = {
-  roots: [
-    "<rootDir>/src"
-  ],
-  transform: {
-    "^.+\\.tsx?$": "ts-jest"
-  },
-  testRegex: "(//.*|(\\.|/)(test|spec|steps))\\.tsx?$",
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node"
-  ],
-  modulePaths: [
-    '<rootDir>/lib'
-  ],
-  collectCoverage: true,
-  coverageDirectory: 'reports',
-  coverageReporters: [
-    'lcov',
-  ],
-  reporters: [
-    "default",
-    "./node_modules/jest-sonarcloud-reporter",
-    [
-      "./node_modules/jest-html-reporter",
-      {
-        "pageTitle": "Test Report",
-        "includeFailureMsg": true,
-        "includeConsoleLog": true
-      }
-    ]
-  ]
+	roots: ["<rootDir>/src"],
+	transform: {
+		"^.+\\.tsx?$": "ts-jest",
+	},
+	testRegex: "(//.*|(\\.|/)(test|spec|steps))\\.tsx?$",
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	modulePaths: ["<rootDir>/lib"],
+	collectCoverage: true,
+	coverageDirectory: "reports",
+	coverageReporters: ["lcov"],
+	reporters: [
+		"default",
+		"./node_modules/jest-sonarcloud-reporter",
+		[
+			"./node_modules/jest-html-reporter",
+			{
+				pageTitle: "Test Report",
+				includeFailureMsg: true,
+				includeConsoleLog: true,
+			},
+		],
+	],
 };
